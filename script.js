de00685a3a73c82c02157aee2e80b8198af9b8ef
Tayo <105395747+Tayo-e@users.noreply.github.com>
2023-11-13 05:30:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 let img1Element = document.querySelector('.img1');
-img1Element.setAttribute('src', '/images/dice' + randomNumber1 + '.png');
+img1Element.setAttribute('src', './images/dice' + randomNumber1 + '.png');
 
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;  // Generate a new random number
 let img2Element = document.querySelector('.img2');
-img2Element.setAttribute('src', '/images/dice' + randomNumber2 + '.png');
+img2Element.setAttribute('src', './images/dice' + randomNumber2 + '.png');
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector('h1').textContent = '🚩 Player One Wins';
